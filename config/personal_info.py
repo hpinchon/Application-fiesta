acb9692a6d5994a794e6610c8a36111f7ace48dc
hpinchon <mrpinchon@gmail.com>
2025-06-10 16:08:56 +0000

    Merge

--- config/personal_info.py
+++ config/personal_info.py
@@ -1,12 +1,12 @@
 # Personal information for applications
 PERSONAL_INFO = {
-    'phone': '+33 7 67 33 80 04',
+    'phone': 'your_phone',
     'address': 'London, UK',
     'visa_status': 'Graduate visa holder',
     'notice_period': '0',
     'salary_expectation': '0',
     'willing_to_relocate': 'Yes',
-    'linkedin_profile': 'https://www.linkedin.com/in/hugo-pinchon-4a3bb2259/'
+    'linkedin_profile': ''
 }
 
 # Standard answers for common application questions
@@ -21,7 +21,7 @@ With my background in {relevant_skills}, I am excited about the opportunity to c
 I look forward to discussing how my skills and enthusiasm can benefit {company}.
 
 Best regards,
-Hugo Pinchon""",
+Your_Name""",
     
     'why_interested': "I am passionate about {industry} and believe {company} offers an excellent opportunity to grow my career while contributing meaningful work.",
     'availability': 'I am available to start immediately',
